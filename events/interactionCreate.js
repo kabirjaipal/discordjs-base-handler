16d36b979abfc7c69e8649f83526e33a2bfaeda3
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+KabirJaipal@users.noreply.github.com>
2021-08-05 07:00:56 +0530

    Update interactionCreate.js
    Updated

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,26 @@
 const client = require("..");
 
-client.on("interactionCreate", async (interaction) => {
-  if (interaction.isCommand()) {
-    await interaction.defer().catch((e) => {
-      console.log(e);
-    });
-    const cmd = client.slashCommands.get(interaction.commandName);
-    if (!cmd) return interaction.followUp({ content: "ERROR, HHAHAHAHA" });
 
-    const args = [];
-    interaction.options.array().map((x) => {
-      args.push(x.value);
-    });
-    cmd.run(client, interaction, args);
-  }
+client.on("interactionCreate", async (interaction) => {
+    // Slash Command Handling
+    if (interaction.isCommand()) {
+        await interaction.defer({ ephemeral: false }).catch(() => {});
+
+        const cmd = client.slashCommands.get(interaction.commandName);
+        if (!cmd)
+            return interaction.followUp({ content: "An error has occured " });
+
+        const args = [];
+
+        for (let option of interaction.options.data) {
+            if (option.type === "SUB_COMMAND") {
+                if (option.name) args.push(option.name);
+                option.options?.forEach((x) => {
+                    if (x.value) args.push(x.value);
+                });
+            } else if (option.value) args.push(option.value);
+        }
+
+        cmd.run(client, interaction, args);
+    }
 });
