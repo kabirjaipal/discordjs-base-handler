267668249edf86483e97b61231535dac77adbe2a
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+KabirJaipal@users.noreply.github.com>
2021-08-15 11:00:29 +0530

    Delete ReconDB.js

--- utils/ReconDB.js
+++
@@ -1,9 +0,0 @@
-const { reconDB } = require("reconlx");
-const config = require('../config/config.json');
-const client = require("..");
-
-const db = new reconDB(client , {
-    uri : config.mongooseConnectionString,
-});
-console.log(`DB Connected`.red);
-module.exports = db;
