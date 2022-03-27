c5f531d6377117181eb9add88b7a525715484939
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-03-27 14:53:44 +0530

    Delete events directory

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-const client = require("..");
-
-client.on('ready', () => {
-    console.log(`${client.user.username} Is Online`);
-    client.user.setActivity(`Kabir Coding me`,{type : "STREAMING", url : "https://twitch.com/tbg"});
-})
