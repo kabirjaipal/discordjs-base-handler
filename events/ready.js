226b5c35f68fc5a7125eee1df45237b9742d7777
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-04-19 07:38:57 +0530

    Delete events directory

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-const client = require("../index");
-
-client.on("ready", async () => {
-  console.log(`${client.user.username} Is Online`);
-  client.user.setActivity({
-    name: `@${client.user.username} /help`,
-    type: "PLAYING",
-  });
-});
