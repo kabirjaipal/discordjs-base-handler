bc4b0c4c7845974ef048c15acb5cb6add4f1f89a
Fire Bird <73753258+kabirsingh2004@users.noreply.github.com>
2023-03-04 00:49:10 +0530

    added more feature ✅
    added more feature ✅

--- settings/config.js
+++ settings/config.js
@@ -1,8 +1,18 @@
+const { Colors } = require("discord.js");
+
 module.exports = {
   TOKEN: process.env.TOKEN || "BOT_TOKEN",
   PREFIX: process.env.PREFIX || "BOT_PREFIX",
   Slash: {
     Global: false,
     GuildID: process.env.GuildID || "GUILD_ID",
+  },
+  embed: {
+    color: Colors.Blurple,
+    wrongColor: Colors.Red,
+  },
+  emoji: {
+    success: "✅",
+    error: "❌",
   },
 };
